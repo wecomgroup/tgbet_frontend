a9b69886662644f204ac407c10cf8927709bd50b
Daniel <ftxbird@gmail.com>
2023-12-20 11:14:38 +0800

    fix:移除无用lib

--- src/util/connect/provider.js
+++
@@ -1,9 +0,0 @@
-import Web3Modal from "web3modal";
-import {getChainData} from "../web3/tool";
-import {providerOptions} from "../web3/config";
-export const provider  = new Web3Modal({
-    theme: 'dark',
-    network: getChainData(1).network,
-    cacheProvider: true,
-    providerOptions,
-})
