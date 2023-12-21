4af61c61d54c3a46c61039ade6dea84f27ed51e7
Daniel <ftxbird@gmail.com>
2023-12-21 18:22:27 +0800

    fix

--- src/components/HomeForm.vue
+++ src/components/HomeForm.vue
@@ -1067,7 +1067,7 @@ export default {
   background-color: rgba(0, 0, 0, 0.8);
   top: 0;
   left: 0;
-  z-index: 999;
+  z-index: 10;
   display: flex;
   align-items: center;
   justify-content: center;
